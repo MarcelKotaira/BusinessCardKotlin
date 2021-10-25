d1cec683548f36f9139fa46ab52115347f62d39c
MarcelKotaira <marcelkotaira@gmail.com>
2021-10-25 18:23:36 -0300

    Addition of "Readme" and update of color names
    ADD
    - Readme
    CHANGE
    - nome das cores
    - seleção da cor de dialog para dropdown

---
+++ README.md
@@ -0,0 +1,15 @@
+# Business Card Kotlin
+
+**App de Cartão de Visitas em Kotlin, para o bootcamp da DIO**
+
+### Descrição da tarefa:
+
+Crie uma aplicação para gerar o seu próprio cartão de visitas pessoal, usando padrões de projeto e boas práticas de programação Kotlin. Tecnicamente, você desenvolverá um App Android nativo para a criação e listagem dos cartões, os quais podem ter os seguintes dados: nome, empresa, telefone de contato, email e cor de fundo. Tudo isso utilizando o Room para armazenar os dados localmente. Por fim, você é desafiado a entregar seu próprio projeto, incorporando suas próprias evoluções e melhorias! Está preparado?
+
+
+
+### Melhorias realizadas
+
+- Adição de um Color Piker para a cor do cartão
+
+- Movido o compartilhamento de cartão de um clique para um clique longo
--- app/src/main/java/br/com/marcelkotaira/businesscardkotlin/colorspinner/ColorList.kt
+++ app/src/main/java/br/com/marcelkotaira/businesscardkotlin/colorspinner/ColorList.kt
@@ -20,18 +20,19 @@ class ColorList
     fun basicColors(): List<ColorObject>
     {
         return listOf(
-            ColorObject("Black", blackHex, whiteHex),
-            ColorObject("Silver", "C0C0C0", blackHex),
-            ColorObject("Gray", "808080", whiteHex),
-            ColorObject("Maroon", "800000", whiteHex),
-            ColorObject("Red", "FF0000", whiteHex),
+            ColorObject("Preto", blackHex, whiteHex),
+            ColorObject("Prata", "C0C0C0", blackHex),
+            ColorObject("Cinza", "808080", whiteHex),
+            ColorObject("Bordo", "800000", whiteHex),
+            ColorObject("Vermelho", "FF0000", whiteHex),
             ColorObject("Fuchsia", "FF00FF", whiteHex),
-            ColorObject("Green", "008000", whiteHex),
+            ColorObject("Rosa", "FFCBDB", blackHex),
+            ColorObject("Verde", "008000", whiteHex),
             ColorObject("Lime", "00FF00", blackHex),
             ColorObject("Olive", "808000", whiteHex),
-            ColorObject("Yellow", "FFFF00", blackHex),
+            ColorObject("Amarelo", "FFFF00", blackHex),
             ColorObject("Navy", "000080", whiteHex),
-            ColorObject("Blue", "0000FF", whiteHex),
+            ColorObject("Azul", "0000FF", whiteHex),
             ColorObject("Teal", "008080", whiteHex),
             ColorObject("Aqua", "00FFFF", blackHex)
         )
